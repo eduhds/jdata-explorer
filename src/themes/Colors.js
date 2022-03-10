e260680bb10c81675f5ecd7302102394c8d800b7
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-03-10 11:43:26 -0300

    Add colors

---
+++ src/themes/Colors.js
@@ -0,0 +1,16 @@
+const Colors = {
+	primary1: '#058bc0',
+	primary2: '#2c5288',
+	primary3: '#f2ae30',
+	primary4: '#f9e4cb',
+	text: '#444444',
+	placeholder: '#C7C7CD',
+	inactive: '#777777',
+	success: '#28A745',
+	danger: '#DC3545',
+	warning: '#FFC107',
+	info: '#17A2B8',
+	white: '#ffffff'
+};
+
+export default Colors;
