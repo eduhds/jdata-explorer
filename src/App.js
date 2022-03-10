263675fc475d9a36dc16984308325b86d52cc065
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-03-10 11:13:00 -0300

    Setup react-native-web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
+import { View, Text } from 'react-native';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<View>
+			<Text>Ola</Text>
+			<img src={logo} className='App-logo' alt='logo' />
+		</View>
+	);
 }
 
 export default App;
