bad3e38927ff2fbc21819cdfc10b449d50f7699f
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-03-12 10:27:34 -0300

    Add Beta info

--- src/navigator/index.jsx
+++ src/navigator/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -17,11 +18,20 @@ export default function Navigator() {
 				screenOptions={{
 					headerTitle: APP_TITLE,
 					headerStyle: { backgroundColor: Colors.primary2 },
-					headerTintColor: Colors.white
+					headerTintColor: Colors.white,
+					headerRight: () => <RightInfo />
 				}}>
 				<Stack.Screen name='Home' component={Home} />
 				<Stack.Screen name='DataExplorer' component={DataExplorer} />
 			</Stack.Navigator>
 		</NavigationContainer>
+	);
+}
+
+function RightInfo() {
+	return (
+		<View style={{ backgroundColor: Colors.primary3, padding: 5, marginRight: 10, borderRadius: 2 }}>
+			<Text style={{ fontSize: 14, color: Colors.white, fontWeight: 'bold' }}>Beta</Text>
+		</View>
 	);
 }
